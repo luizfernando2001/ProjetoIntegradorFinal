f970117dc8346feef835a0771d6b893032155fe5
kemelly nasc <kemellynasc@gmail.com>
2020-12-03 16:50:50 -0300

    Layout do site. Adicionando site externo e sua estrutura.

---
+++ app/site/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Final</title>
+
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="app/assets/css/bootstrap.min.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="app/assets/img/footerphoto.gif">
+    <!-- Icomoon -->
+    <link rel="stylesheet" href="app/assets/img/icons/icomoon.css">
+    <!-- Meu CSS -->
+    <link rel="stylesheet" href="app/assets/css/projetofinalcss.css">
+</head>
+
+<body>
+
+
+    <!-- Header -->
+
+    <header class="container">
+        <img src="https://placehold.it/1140x250" alt="" class="">
+    </header>
+
+ 
