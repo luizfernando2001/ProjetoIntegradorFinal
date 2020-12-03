f3ec71de8f6deee341f34c730f7745ccde9c6ec6
edsay12 <edvandearaujo2@hotmail.com>
2020-12-03 16:38:45 -0300

    adicinando painel adm

---
+++ cpanel.php
@@ -0,0 +1,87 @@
+<?php
+
+
+
+
+
+
+
+
+
+$cpanel = isset($_GET["pg"]);
+
+
+
+
+if ($cpanel) {
+
+    switch ($_GET["pg"]) {
+
+
+        case 'agenda':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/agenda.php";
+
+            include_once "app/painelAdm/paginas/includes/footer.php";
+
+
+            break;
+        case 'cadastrodemedicos':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/cadastrodemedicos.php";
+
+            include_once "app/painelAdm/paginas/includes/footer.php";
+
+
+            break;
+        case 'profissionais':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/profissionais.php";
+
+            include_once "app/painelAdm/paginas/includes/footer.php";
+
+
+            break;
+        case 'pacientes':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/pacientes.php";
+
+            include_once "app/painelAdm/paginas/includes/footer.php";
+
+            break;
+
+        case 'marcarconsulta':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/marcarconsulta.php";
+
+            include_once "app/painelAdm/paginas/includes/footer.php";
+
+            break;
+
+
+        case 'cadastrodeespecialidades':
+            include_once "app/painelAdm/paginas/includes/header.php";
+            include_once "app/painelAdm/paginas/includes/navegacao.php";
+            include_once "app/painelAdm/paginas/cadastrodeespecialidades.php";
+
+            include_once "app/painelAdm/paginas/includes/footer.php";
+
+
+            break;
+
+        default:
+
+            break;
+    }
+} else {
+    include_once "app/painelAdm/paginas/includes/header.php";
+    include_once "app/painelAdm/paginas/includes/navegacao.php";
+    include_once "app/painelAdm/paginas/inicial.php";
+
+    include_once "app/painelAdm/paginas/includes/footer.php";
+}
