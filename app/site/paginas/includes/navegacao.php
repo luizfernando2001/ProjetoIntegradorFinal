c66fb0550b4a3401852f4f06131a43f7b33c8ea3
kemelly nasc <kemellynasc@gmail.com>
2020-12-09 15:34:08 -0300

    adicionando header, footer e conteúdo de páginas.

--- app/site/paginas/includes/navegacao.php
+++ app/site/paginas/includes/navegacao.php
@@ -1,4 +1,3 @@
-
 <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
@@ -7,24 +6,19 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="app/assets/img/banner/banner8.jpg" class="d-block w-100" alt="...">
+      <img src="app/assets/img/banner/banner13.jfif" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h2>Clínica escola</h2>
-        <h3 class="font-italic bg-success">Odontologista</h3>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="app/assets/img/banner/banner8.jpg" class="d-block w-100" alt="...">
+      <img src="app/assets/img/banner/banner14.jfif" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h3>Psicólogo</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
       </div>
     </div>
     <div class="carousel-item">
-      <img src="app/assets/img/banner/banner8.jpg" class="d-block w-100" alt="...">
+      <img src="app/assets/img/banner/banner15.jfif" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h3>Urologista</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
       </div>
     </div>
   </div>
@@ -37,77 +31,43 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-<br><br><br><br>
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <div class="row">
-     <!-- card slide aq  -->
-     <div class="card col ml-5" style="height: 300px;">
-  <div class="card-body">
-    <h5 class="card-title">Cardiologista</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
 
-<div class="card col mx-5" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Psiquiatra</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
 
-<div class="card col mr-5" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Psicólogo</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-</div>
-    </div>
+<!-- Agendamento -->
 
-    
-    <div class="carousel-item">
-  <!-- card slide aq  -->
+<div id="navega" class="container-fluid">
   <div class="row">
-  <div class="card col ml-5" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Odontologista</h5>
-
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  
-  </div>
-</div>
-<div class="card col mx-5" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Urologista</h5>
- 
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-  </div>
-</div>
-<div class="card col mr-5" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Pediatra</h5>
-
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-  </div>
-</div>
+    <div class="col-12 text-sm-center col-md-5">
+      <img class="mao mr-5" src="app/assets/img/navegacao/mao.png">
     </div>
- 
+    <div class="col-12 m-4 m-md-0 col-md-5">
+      <div id="agenda" class="row">
+        <div class="col mb-5 ml-5 pl-5">
+          <h2 class=""> <span class="icon-event_note ml-5"></span>
+            Agendamento Online</h2>
+          <p class="paragraph mx-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eum reprehenderit aspernatur debitis facere facilis rerum, adipisci et, quibusdam sed accusamus architecto velit! Reprehenderit maiores earum expedita tempore vitae similique.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eum reprehenderit aspernatur debitis facere facilis rerum, adipisci et, quibusdam sed accusamus architecto velit! Reprehenderit maiores earum expedita tempore vitae similique..</p>
+        </div>
+
+      </div>
+    </div>
   </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-   
-<br>
- <!-- informações de consulta  -->
+
+<!-- Fim agendamento -->
+
+<!-- Plano de saúde -->
+<div id="agenda2" class="container-fluid">
+  <div class="row d-flex justify-content-around">
+    <div class="col-12 m-4 m-md-0 col-md-5">
+      <div>
+        <h2>Vínculos com planos de saúde</h2>
+        <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eum reprehenderit aspernatur debitis facere facilis rerum, adipisci et, quibusdam sed accusamus architecto velit! Reprehenderit maiores earum expedita tempore vitae similique..</p>
+      </div>
+    </div>
+    <div class="col-12 text-sm-center col-md-5">
+      <img src="app/assets/img/navegacao/planos.png" width="" height="500" class="img-fluid" />
+    </div>
+  </div>
+</div>
+<!-- Fim plano de saúde -->
