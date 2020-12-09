f48e9d843700975f7e9f76e5983ddedaa3189c42
kemelly nasc <kemellynasc@gmail.com>
2020-12-09 16:45:08 -0300

    Ajustes sla

---
+++ app/site/paginas/inicial.php
@@ -0,0 +1,37 @@
+<!-- Agendamento -->
+
+<div id="navega" class="container-fluid">
+  <div class="row">
+    <div class="col-12 text-sm-center col-md-5">
+      <img class="mao mr-5" src="app/assets/img/navegacao/mao.png">
+    </div>
+    <div class="col-12 m-4 m-md-0 col-md-5">
+      <div id="agenda" class="row">
+        <div class="col mb-5 ml-5 pl-5">
+          <h2 class=""> <span class="icon-event_note ml-5"></span>
+            Agendamento Online</h2>
+          <p class="paragraph mx-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eum reprehenderit aspernatur debitis facere facilis rerum, adipisci et, quibusdam sed accusamus architecto velit! Reprehenderit maiores earum expedita tempore vitae similique.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eum reprehenderit aspernatur debitis facere facilis rerum, adipisci et, quibusdam sed accusamus architecto velit! Reprehenderit maiores earum expedita tempore vitae similique..</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Fim agendamento -->
+
+<!-- Plano de saúde -->
+<div id="agenda2" class="container-fluid">
+  <div class="row d-flex justify-content-around">
+    <div class="col-12 m-4 m-md-0 col-md-5">
+      <div>
+        <h2>Vínculos com planos de saúde</h2>
+        <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eum reprehenderit aspernatur debitis facere facilis rerum, adipisci et, quibusdam sed accusamus architecto velit! Reprehenderit maiores earum expedita tempore vitae similique..</p>
+      </div>
+    </div>
+    <div class="col-12 text-sm-center col-md-5">
+      <img src="app/assets/img/navegacao/planos.png" width="" height="500" class="img-fluid" />
+    </div>
+  </div>
+</div>
+<!-- Fim plano de saúde -->
